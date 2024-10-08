58a6ff326c0e0d608838544c7b376c675b6644f2
MartinKemppi <dusperin1234@gmail.com>
2024-10-08 13:56:34 +0300

    Add project files.

---
+++ Veebirakenduste loomine API MartinKemppi/Controllers/PrimitiividController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+
+namespace Veebirakenduste_loomine_API_MartinKemppi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PrimitiividController : ControllerBase
+    {        
+        // GET: primitiivid/hello-world
+        [HttpGet("hello-world")]
+        public string HelloWorld()
+        {
+            return "Hello world at " + DateTime.Now;
+        }
+
+        // GET: primitiivid/hello-variable/mari
+        [HttpGet("hello-variable/{nimi}")]
+        public string HelloVariable(string nimi)
+        {
+            return "Hello " + nimi;
+        }
+
+        // GET: primitiivid/add/5/6
+        [HttpGet("add/{nr1}/{nr2}")]
+        public int AddNumbers(int nr1, int nr2)
+        {
+            return nr1 + nr2;
+        }
+
+        // GET: primitiivid/multiply/5/6
+        [HttpGet("multiply/{nr1}/{nr2}")]
+        public int Multiply(int nr1, int nr2)
+        {
+            return nr1 * nr2;
+        }
+
+        // GET: primitiivid/do-logs/5
+        [HttpGet("do-logs/{arv}")]
+        public void DoLogs(int arv)
+        {
+            for (int i = 0; i < arv; i++)
+            {
+                Console.WriteLine("See on logi nr " + i);
+            }
+        }
+
+        // GET: primitiivid/aasta/2000
+        [HttpGet("aasta/{nr1}")]
+        public string Synniaasta(string nr1)
+        {
+            string AA1970 = "Sina oled vana";
+            string AA2000 = "Sina oled noor";
+            string AA2010 = "Sina oled väga noor";
+            string NoData = "Palun, sisesta arv";
+
+            int numericvalue;
+            bool isNumber = int.TryParse(nr1, out numericvalue);
+
+            if (!isNumber)
+            {
+                return NoData;
+            }
+
+            if (numericvalue < 1970)
+            {
+                return AA1970;
+            }
+            else if (numericvalue < 2000)
+            {
+                return AA2000;
+            }
+            else if (numericvalue < 2010)
+            {
+                return AA2010;
+            }
+            else
+            {
+                return NoData;
+            }
+        }
+
+        // GET: primitiivid/paar-arvud
+        [HttpGet("paar-arvud")]
+        public string Paararvud()
+        {
+            Random rand = new Random();
+
+            int Nr1 = rand.Next(1,100);
+            int Nr2 = rand.Next(1,100);
+
+            return $"Arvud on: {Nr1} ja {Nr2}";
+
+        }
+    }
+}
---
+++ Veebirakenduste loomine API MartinKemppi/Controllers/ToodeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Veebirakenduste_loomine_API_MartinKemppi.Models;
+
+namespace Veebirakenduste_loomine_API_MartinKemppi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ToodeController : ControllerBase
+    {
+        private static Toode _toode = new Toode(1, "Koola", 1.5, true);
+
+        // GET: toode
+        [HttpGet]
+        public Toode GetToode()
+        {
+            return _toode;
+        }
+
+        // GET: toode/suurenda-hinda
+        [HttpGet("suurenda-hinda")]
+        public Toode SuurendaHinda()
+        {
+            _toode.Price = _toode.Price + 1;
+            return _toode;
+        }
+
+        // GET: toode/false-true-false
+        [HttpGet("false-true-false")]
+        public Toode FalseTrueFalse()
+        {
+            if(_toode.IsActive == true)
+            {
+                _toode.IsActive = _toode.IsActive = false;
+            }
+            else
+            {
+                _toode.IsActive = _toode.IsActive = true;
+            }               
+            return _toode;
+        }
+
+        // GET: toode/ümbernimetamine
+        [HttpGet("ümbernimetamine")]
+        public Toode FalseTrueFalse(string Name)
+        {
+            _toode.Name = Name;
+
+            return _toode;
+        }
+
+        // GET: toode/uushind
+        [HttpGet("uushind")]
+        public Toode Uushind(float Xkorda)
+        {
+            _toode.Price *= Xkorda;
+
+            return _toode;
+        }
+    }
+}
