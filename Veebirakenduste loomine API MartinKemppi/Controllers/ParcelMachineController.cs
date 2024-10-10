982ce7788c151cef99875b54262b72675760e853
MartinKemppi <dusperin1234@gmail.com>
2024-10-10 11:05:52 +0300

    Uuendatud version
    Nordpool controller
    ParcelMachine controller
    Payment controller
    Tooded controller
    Programm uuendatud
    Word uuendatud ning lisatud täpsus piltidele

---
+++ Veebirakenduste loomine API MartinKemppi/Controllers/ParcelMachineController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Veebirakenduste_loomine_API_MartinKemppi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ParcelMachineController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+
+        public ParcelMachineController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [HttpGet("omniva")]
+        public async Task<IActionResult> GetParcelMachinesOmniva()
+        {
+            var response = await _httpClient.GetAsync("https://www.omniva.ee/locations.json");
+            var responseBody = await response.Content.ReadAsStringAsync();
+            return Content(responseBody, "application/json");
+        }
+
+        [HttpGet("smartpost")]
+        public async Task<IActionResult> GetParcelMachinesSmartPost()
+        {
+            var response = await _httpClient.GetAsync("https://www.smartpost.ee/places.json");
+            var responseBody = await response.Content.ReadAsStringAsync();
+            return Content(responseBody, "application/json");
+        }
+    }
+}
--- Veebirakenduste loomine API MartinKemppi/Controllers/TootedControlle.cs
+++ Veebirakenduste loomine API MartinKemppi/Controllers/TootedControlle.cs
@@ -8,37 +8,39 @@ namespace Veebirakenduste_loomine_API_MartinKemppi.Controllers
     [ApiController]
     public class TootedController : ControllerBase
     {
-        private static List<Toode> _tooted = new List<Toode>{
-        new Toode(1,"Koola", 1.5, true),
-        new Toode(2,"Fanta", 1.0, false),
-        new Toode(3,"Sprite", 1.7, true),
-        new Toode(4,"Vichy", 2.0, true),
-        new Toode(5,"Vitamin well", 2.5, true)
+        private static List<Toode> _tooted = new()
+        {
+            new Toode(1,"Koola", 1.5, true),
+            new Toode(2,"Fanta", 1.0, false),
+            new Toode(3,"Sprite", 1.7, true),
+            new Toode(4,"Vichy", 2.0, true),
+            new Toode(5,"Vitamin well", 2.5, true)
         };
 
-
-        // https://localhost:port/tooted
+        // GET https://localhost:4444/tooted
         [HttpGet]
         public List<Toode> Get()
         {
             return _tooted;
         }
 
-        [HttpGet("kustuta/{index}")]
+        // DELETE https://localhost:4444/tooted/kustuta/0
+        [HttpDelete("kustuta/{index}")]
         public List<Toode> Delete(int index)
         {
             _tooted.RemoveAt(index);
             return _tooted;
         }
 
-        [HttpGet("kustuta2/{index}")]
+        [HttpDelete("kustuta2/{index}")]
         public string Delete2(int index)
         {
             _tooted.RemoveAt(index);
             return "Kustutatud!";
         }
 
-        [HttpGet("lisa/{id}/{nimi}/{hind}/{aktiivne}")]
+        // POST https://localhost:4444/tooted/lisa/1/Coca/1.5/true
+        [HttpPost("lisa/{id}/{nimi}/{hind}/{aktiivne}")]
         public List<Toode> Add(int id, string nimi, double hind, bool aktiivne)
         {
             Toode toode = new Toode(id, nimi, hind, aktiivne);
@@ -46,16 +48,17 @@ namespace Veebirakenduste_loomine_API_MartinKemppi.Controllers
             return _tooted;
         }
 
-        [HttpGet("lisa")] // GET /tooted/lisa?id=1&nimi=Koola&hind=1.5&aktiivne=true
-        public List<Toode> Add2([FromQuery] int id, [FromQuery] string nimi, [FromQuery] double hind, [FromQuery] bool aktiivne)
+        [HttpPost("lisa2")]
+        public List<Toode> Add2(int id, string nimi, double hind, bool aktiivne)
         {
             Toode toode = new Toode(id, nimi, hind, aktiivne);
             _tooted.Add(toode);
             return _tooted;
         }
 
-        [HttpGet("hind-dollaritesse/{kurss}")] // GET /tooted/hind-dollaritesse/1.5
-        public List<Toode> Dollaritesse(double kurss)
+        // PATCH https://localhost:4444/tooted/hind-dollaritesse/1.5
+        [HttpPatch("hind-dollaritesse/{kurss}")]
+        public List<Toode> UpdatePrices(double kurss)
         {
             for (int i = 0; i < _tooted.Count; i++)
             {
@@ -74,6 +77,13 @@ namespace Veebirakenduste_loomine_API_MartinKemppi.Controllers
                 t.Price = t.Price * kurss;
             }
 
+            return _tooted;
+        }
+
+        [HttpPost("lisa")]
+        public List<Toode> Add([FromBody] Toode toode)
+        {
+            _tooted.Add(toode);
             return _tooted;
         }
 
@@ -107,6 +117,23 @@ namespace Veebirakenduste_loomine_API_MartinKemppi.Controllers
         {
             var toode = _tooted.OrderByDescending(t => t.Price).First();
             return toode != null ? toode : NotFound();
+        }
+
+        // PUT https://localhost:port/tooted/uuenda/{id}
+        [HttpPut("uuenda/{id}")]
+        public ActionResult<Toode> Update(int id, [FromBody] Toode updatedToode)
+        {
+            var Toode = _tooted.Find(t => t.Id == id);
+            if (Toode == null)
+            {
+                return NotFound();
+            }
+
+            Toode.Name = updatedToode.Name;
+            Toode.Price = updatedToode.Price;
+            Toode.IsActive = updatedToode.IsActive;
+
+            return Toode;
         }
 
     }
