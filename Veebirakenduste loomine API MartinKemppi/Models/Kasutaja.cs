a3507a6d9d25edb5e0d4ba34c1c8e09bafeb2f51
MartinKemppi <dusperin1234@gmail.com>
2024-10-13 09:07:01 +0300

    Kasutaja
    mudeli kasutaja loomine

---
+++ Veebirakenduste loomine API MartinKemppi/Models/Kasutaja.cs
@@ -0,0 +1,20 @@
+﻿namespace Veebirakenduste_loomine_API_MartinKemppi.Models
+{
+    public class Kasutaja
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+        public Kasutaja(int id, string username, string password, string firstname, string lastname)
+        {
+            Id = id;
+            Username = username;
+            Password = password;
+            Firstname = firstname;
+            Lastname = lastname;
+        }
+    }
+}
